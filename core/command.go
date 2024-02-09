38e89ec224d767e4619ac856856ae3aa027c21f4
martoc <martoc@users.noreply.github.com>
2024-02-09 14:43:11 +0000

    docs: Added documentation

--- core/command.go
+++ core/command.go
@@ -1,5 +1,7 @@
 package core
 
+// Command is an interface that represents a command.
+// It defines the Execute method, which executes the command and returns a string and an error.
 type Command interface {
 	Execute() (string, error)
 }
