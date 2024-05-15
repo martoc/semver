d73b4b7d06d9731dded5af5495ce9e773d5bd5ae
martoc <octaviomartin@gmail.com>
2024-05-15 21:10:30 +0100

    fix: the input device is not a TTY

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -6,9 +6,9 @@ test_files=$(ls ./integration-tests/*.bats)
 # Iterate over each test file and run it
 for file in $test_files; do
   echo "Running tests in $file"
-  docker run -it -v "${PWD}:/workspace" \
+  docker run -v "${PWD}:/workspace" \
     -e INTEGRATION_TEST_ROOT=/workspace \
     -e GOOS=linux \
     -e GOARCH=$(go env GOARCH) \
-    bats/bats:latest /workspace/integration-tests/$(basename "$file")
+    bats/bats:1.11.0 /workspace/integration-tests/$(basename "$file")
 done
